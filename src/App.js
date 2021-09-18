ac1ac8243b77877884d7ed3eb5caa48ac3109ab7
kang hyeon jun <rkd980609@naver.com>
2021-09-18 14:26:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@
 function App() {
   return (
     <div>
-      hello react!!
+      <h1>Hello</h1>
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
     </div>
   );
+}
+function Movie(){
+  return <h1>I like potato</h1>
 }
 
 export default App;
