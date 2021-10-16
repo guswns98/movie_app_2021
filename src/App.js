55acbbb96fb04ae2f50f62a6d982451d74eff205
kang hyeon jun <rkd980609@naver.com>
2021-10-16 15:57:53 +0900

    1013-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react"
 import axios from "axios"
+import Movie from './Movie'
+import './App.css'
 
 class App extends React.Component {
     state = {
@@ -12,8 +14,8 @@ class App extends React.Component {
             data: {
                 data: {movies},
             },
-        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-        console.log(movies)
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+        this.setState({movies, isLoading: false})
     }
 
     componentDidMount() {
@@ -21,11 +23,29 @@ class App extends React.Component {
     }
 
     render() {
-        const { isLoading } = this.state
+        const { isLoading, movies } = this.state
         return (
-            <div>
-                { isLoading ? 'Loading...' : '영화 데이터 출력' }
-            </div>
+            <section className="container">
+                { isLoading ? (
+                    <div className="loader">
+                        <span className="loader-text">Loading...</span>
+                    </div>
+                    ) : (
+                        <div className="movies">
+                        {movies.map((movie) => (
+                    <Movie 
+                        key={movie.id}
+                        id={movie.id}
+                        year={movie.year}
+                        title={movie.title}
+                        summary={movie.summary}
+                        poster={movie.medium_cover_image}
+                        genres={movie.genres}
+                    />
+                ))}
+                </div>
+                )}
+            </section>
         )
     }
 }
