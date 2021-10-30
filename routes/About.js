c24665b041df9cbfa6aa45ea5ab8b97d7eafac58
kang hyeon jun <rkd980609@naver.com>
2021-10-30 21:56:14 +0900

    1027_2

---
+++ routes/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css'
+
+function About() {
+    return(
+        <div className="about-container">
+            <span>
+                "hahahahaha"
+            </span>
+            <span>- ahahahah</span>
+        </div>
+    );
+}
+
+export default About;
